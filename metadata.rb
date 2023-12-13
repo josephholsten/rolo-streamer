ada0db264c25b017f89399d57b5c7413886d1527
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-12-13 20:50:36 +0000

    streamer: init

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name 'travalio'
+name 'streamer'
 version '0.1.0'
 
 depends 'base'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,20 @@
 # base system
 include_recipe 'base::default'
 
-# workstation configuration
-include_recipe 'travalio::utensiles'
+package 'docker-compose'
+
+directory '/etc/apache2' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+template 'docker-compose.yaml' do
+  source 'docker-compose.yaml'
+
+  path '/srv/containers/docker-compose.yaml'
+  mode '0755'
+  owner 'web_admin'
+  group 'web_admin'
+end
